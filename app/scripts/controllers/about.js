0916ec2f43020958cb62069179539dd57b5b180d
Artem Tabalin <tabalinas@gmail.com>
2015-03-09 10:30:41 +0300

    Structure: Yeoman angular generator

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name algolandnetApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the algolandnetApp
+ */
+angular.module('algolandnetApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name algolandnetApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the algolandnetApp
+ */
+angular.module('algolandnetApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
