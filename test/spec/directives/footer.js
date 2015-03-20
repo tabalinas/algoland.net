15b67c4850d6bbc871b5ff7038d0427c832f2d72
Artem Tabalin <tabalinas@gmail.com>
2015-03-20 09:58:18 +0300

    Tests: Add al.footer directive tests

---
+++ test/spec/directives/footer.js
@@ -0,0 +1,48 @@
+"use strict";
+
+describe("al.footer", function() {
+
+    var element;
+    var $compile;
+    var scope;
+
+    var createDirective = function() {
+        element = angular.element("<al-footer></al-footer>");
+        $compile(element)(scope);
+        scope.$digest();
+    };
+
+
+    beforeEach(module("algoland"));
+    beforeEach(module("partials/footer.html"));
+
+    beforeEach(inject(function(_$compile_, $rootScope, appConfig) {
+        $compile = _$compile_;
+        scope = $rootScope.$new();
+    }));
+
+    it("should render link to github", inject(function(appConfig) {
+        var testAccount = "github_test";
+        appConfig.github = testAccount;
+
+        createDirective();
+
+        var link = element.find(".footer-link-github");
+        expect(link.text()).toBe(testAccount);
+        expect(link.attr("href")).toBe("https://github.com/" + testAccount);
+    }));
+
+    it("should render link to twitter", inject(function(appConfig) {
+        var testAccount = "twitter_test";
+        appConfig.twitter = testAccount;
+
+        createDirective();
+
+        var link = element.find(".footer-link-twitter");
+        expect(link.text()).toBe("@" + testAccount);
+        expect(link.attr("href")).toBe("https://twitter.com/" + testAccount);
+    }));
+
+});
+
+
