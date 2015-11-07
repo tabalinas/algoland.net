4cd75cc921b40173ed9f8be55a0ca1776a9e4438
Artem Tabalin <tabalinas@gmail.com>
2015-11-08 00:33:40 +0100

    Algos: Improve algorithms list appearence

--- app/scripts/directives/algo.js
+++ app/scripts/directives/algo.js
@@ -5,7 +5,14 @@ angular.module("algoland")
         return {
             restrict: "E",
             replace: true,
-            templateUrl: "partials/algo.html"
+            templateUrl: "partials/algo.html",
+            scope: {
+                showCategory: '=',
+                algo: '='
+            },
+            link: function(scope, element, attrs){
+                scope.showCategory = scope.showCategory !== undefined ? scope.showCategory : true;
+            }
         };
     });
 
